589efcdbe1296bc22c04b246d6359d5936244e71
Ian Burke <iano.burke@gmail.com>
2017-11-03 21:58:37 +0000

    Added templates folder for html files. Index page up and running.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 # Import flask 
 
 # Pass in __name__ to help flask determine root path
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # @ signifies a decorator which is a way to wrap a function and modify its behaviour
 @app.route("/") # Connect to webpage. "/" is a root directory
 def main():
-    return "Hello World!"
+    return render_template('index.html')
 
  
 if __name__ == "__main__":
