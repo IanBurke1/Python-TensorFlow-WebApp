07102a2bf819dd38cfb855bb15bebd030665d385
Ian Burke <iano.burke@gmail.com>
2017-11-03 21:13:40 +0000

    missing extra underscore in "__main__" fixed.  App.py up and running

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 # Routing/Mapping
 # @ signifies a decorator which is a way to wrap a function and modify its behaviour
 @app.route("/") # Connect to webpage. "/" is a root directory
-def hello():
+def main():
     return "Hello World!"
 
-# 
-if __name__ == "_main_":
+ 
+if __name__ == "__main__":
     app.run() # Start the web server
