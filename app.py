45e5df83c2fc2210dbb577af0cb5492a6509cd02
Ian Burke <iano.burke@gmail.com>
2017-11-06 16:57:03 +0000

    Added file upload form

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 # Import flask 
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # Pass in __name__ to help flask determine root path
 app = Flask(__name__)
 
-# Routing/Mapping
-# @ signifies a decorator which is a way to wrap a function and modify its behaviour
-@app.route("/") # Connect to webpage. "/" is a root directory
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/') #connect a webpage. '/' is a root directory.
 def main():
+
     return render_template("index.html")
 
- 
+# Routing/Mapping
+# @ signifies a decorator which is a way to wrap a function and modify its behaviour
+@app.route("/", methods=['GET', 'POST']) # Connect to webpage. "/" is a root directory
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
 if __name__ == "__main__":
     app.run() # Start the web server
