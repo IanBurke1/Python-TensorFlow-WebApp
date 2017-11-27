5f32b74c1e10b42438f61af1c44a3a94f3460e08
Ian Burke <iano.burke@gmail.com>
2017-11-27 16:02:14 +0000

    Upload images working

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
-# Import flask 
 
 # UPLOAD_FOLDER is where we will store the uploaded files
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = './static/uploads'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif']) # set of allowed file extensions.
 
 # Pass in __name__ to help flask determine root path
 app = Flask(__name__) # Initialising flask app
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
 def allowed_file(filename):
@@ -18,11 +18,11 @@ def allowed_file(filename):
 @app.route('/') #connect a webpage. '/' is a root directory.
 def main():
 
-    return render_template("index.html")
+   return render_template("index.html")
 
 # Routing/Mapping
 # @ signifies a decorator which is a way to wrap a function and modify its behaviour
-@app.route("/", methods=['GET', 'POST']) # Connect to webpage. "/" is a root directory
+@app.route("/upload", methods=['GET', 'POST']) # Connect to webpage. "/" is a root directory
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -36,15 +36,18 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            filename = secure_filename(file.filename) # secure a filename before storing it directly
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return
+    return render_template('index.html')
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True) # Start the web server. debug=True means to auto refresh page after code changes 
