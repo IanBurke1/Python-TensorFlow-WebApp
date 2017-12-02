7680b2715fd9f6de955b32e148b69b98d3199136
Ian Burke <iano.burke@gmail.com>
2017-12-02 18:01:27 +0000

    Added app.py comments

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-import os
 from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
-from load import *
-from skimage import color
-from scipy.misc import imread, imresize, imsave
-import numpy as np
-import re
-import base64
+from werkzeug.utils import secure_filename 
+import os
+from load import * 
+from skimage import color # change colour of image
+from scipy.misc import imread, imresize, imsave # For images
+import numpy as np # martix math
+import re #regular expression for canvas img string data
+import base64 # Encode canvas data bytes
 
-# Upload files adapted from: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+# Upload files code adapted from: http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+# Canvas image code adapted from: https://github.com/llSourcell/how_to_deploy_a_keras_model_to_production/blob/master/app.py
 
-# UPLOAD_FOLDER is where we will store the uploaded files
+# =========== Initialisation ============================================================================================================
+
+# UPLOAD_FOLDER is where we will store the uploaded image files
 UPLOAD_FOLDER = './static/uploads'
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif']) # set of allowed file extensions.
+# set of allowed file extensions.
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif']) 
 
+# model and graph from load.py
 model, graph = loadModel()
 
 # Pass in __name__ to help flask determine root path
 app = Flask(__name__) # Initialising flask app
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER # configure upload folder
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+# =============== Routing/Mapping =======================================================================================================
 
+# @ signifies a decorator which is a way to wrap a function and modify its behaviour
+@app.route('/') #connect a webpage. '/' is a root directory.
+def main():
+   return render_template("index.html") # return rendered template 
+
+# ============== Upload image ====================
+
+# function that checks if file extension is allowed
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
-@app.route('/') #connect a webpage. '/' is a root directory.
-def main():
-   return render_template("index.html")
-
-# Routing/Mapping
-# @ signifies a decorator which is a way to wrap a function and modify its behaviour
-@app.route("/upload", methods=['GET', 'POST']) # Connect to webpage. "/" is a root directory
+# Upload Image file
+@app.route("/upload", methods=['GET', 'POST']) 
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -45,41 +53,40 @@ def upload_file():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        # if theres a file with allowed extension then..
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename) # secure a filename before storing it directly
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-           # x = color.rgb2gray(imread(filename, mode='L')) # change colour to greyscale
-           #  x = imresize(x, (28,28)) # resize the image 
-           # x = x.reshape(1, 28, 28, 1) # Reshape the image
-           # with graph.as_default():
-            #    out = model.predict(x)
-             #   print(out)
-              #  print(np.argmax(out, axis=1))
-               # response = np.array_str(np.argmax(out, axis=1))
-                #return response
+            # secure a filename before storing it directly
+            filename = secure_filename(file.filename) 
+            # Save file to upload_folder
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename)) 
            
             return redirect(url_for('uploaded_file', filename=filename))
     
    # return render_template('index.html')
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+#@app.route('/uploads/<filename>')
+#def uploaded_file(filename):
+#    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
-def getImage(imgData):
+# ============== Canvas Image ========================
+
+def convertData(imgData):
     # parse canvas bytes and save as output.png
+    # base64 is used to take binary data and turn it into text to easily transmit from html
     imgstr = re.search(b'base64,(.*)', imgData).group(1)
     with open('output.png','wb') as output:
         output.write(base64.decodebytes(imgstr))
 
+# Canvas Digit Image
 @app.route('/predict',methods=['GET','POST'])
 def predict():
         #Get data from canvas and save as image
-        getImage(request.get_data())
-        # read parsed image back in 8-bit, black and white mode (L)
+        convertData(request.get_data())
+        # read parsed image back in mode L = 8-bit pixels, black and white.
         x = imread('output.png',mode='L')
+        # compute a bit-wise inversion
         x = np.invert(x)
-        # make it the right size
+        # make it 28x28
         x = imresize(x,(28,28))
         
         #convert to a 4D tensor to feed into our neural network model
@@ -87,7 +94,7 @@ def predict():
         
         #in our computation graph
         with graph.as_default():
-            #perform the prediction
+            # predict the digit using our model
             prediction = model.predict(x)
             print(prediction)
             print(np.argmax(prediction,axis=1))
